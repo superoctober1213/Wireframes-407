cc94a426390c273e0375fd3d31fc87b46dea4e95
Emma Liu <liu763@wisc.edu>
2020-05-02 23:18:35 -0400

    Implemented UI pages, and created a throw bottle animation.

--- app/src/main/java/com/example/wireframes_407/ThrowBottle.java
+++ app/src/main/java/com/example/wireframes_407/ThrowBottle.java
@@ -7,10 +7,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
 import com.example.wireframes_407.DTBBottle;
 
 public class ThrowBottle extends AppCompatActivity {
@@ -19,6 +22,8 @@ public class ThrowBottle extends AppCompatActivity {
     Button button;
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
 
+    AnimationDrawable throwAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +31,9 @@ public class ThrowBottle extends AppCompatActivity {
 
         message = findViewById(R.id.context);
         button = findViewById(R.id.throwButton);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+        imageView.setBackgroundResource(R.drawable.animation);
+        throwAnimation = (AnimationDrawable) imageView.getBackground();
     }
 
     public void sendItFlowing(View view){
@@ -34,6 +42,8 @@ public class ThrowBottle extends AppCompatActivity {
         String  gender = MainActivity.getGender();
         String content = message.toString();
         DTBBottle.throwBottle(usernameF, timestamp, gender, content);
+
+        throwAnimation.start();
 
         Intent back = new Intent(this,LoginActivity.class);
         startActivity(back);
